82c274e5999d6865ea86b36a0e8faa057ef7f618
EREN SAVASIR <erensavasir47@gmail.com>
2024-03-31 03:06:05 +0300

    Mvc Ders 11 Controller Ve Navbar

---
+++ Mvcstok/Controllers/KategoritController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Mvcstok.Controllers
+{
+    public class KategoritController : Controller
+    {
+        // GET: Kategorit
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
