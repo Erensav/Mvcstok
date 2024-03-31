e31afa83448cd1b77f7247ff4bcfb8ae1ac220d3
EREN SAVASIR <erensavasir47@gmail.com>
2024-03-31 03:54:57 +0300

    Mvc Ders 15 Müşteri Listeleme

---
+++ Mvcstok/Controllers/MusteriController.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using Mvcstok.Models.Entity;
+
+namespace Mvcstok.Controllers
+{
+    public class MusteriController : Controller
+    {
+        // GET: Musteri
+        MvcdpstokEntities db = new MvcdpstokEntities();
+        public ActionResult Index()
+        {
+            var degerler = db.tbl_musteriler.ToList();
+            return View(degerler);
+        }
+    }
+}
