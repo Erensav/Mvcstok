dc8152223a9770f91f3433053ac8cb66013a28b4
EREN SAVASIR <erensavasir47@gmail.com>
2024-03-31 03:36:05 +0300

    Mvc Ders 14 Ürünleri Listeleme

---
+++ Mvcstok/Controllers/UrunController.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using Mvcstok.Models.Entity;
+
+namespace Mvcstok.Controllers
+{
+    public class UrunController : Controller
+    {
+        // GET: Urun
+        MvcdpstokEntities db = new MvcdpstokEntities();
+        public ActionResult Index()
+        {
+            var degerler =db.tbl_urunler.ToList();
+            return View(degerler);
+        }
+    }
+}
