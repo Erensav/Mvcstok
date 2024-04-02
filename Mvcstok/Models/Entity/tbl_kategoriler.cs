9347b83cd6e57e91e1366ee44b9459b4b723f2ba
EREN SAVASIR <erensavasir47@gmail.com>
2024-04-02 13:48:23 +0300

    Mvc Ders 34 Validation Kontrolleri Required

--- Mvcstok/Models/Entity/tbl_kategoriler.cs
+++ Mvcstok/Models/Entity/tbl_kategoriler.cs
@@ -11,7 +11,8 @@ namespace Mvcstok.Models.Entity
 {
     using System;
     using System.Collections.Generic;
-    
+    using System.ComponentModel.DataAnnotations;
+
     public partial class tbl_kategoriler
     {
         [System.Diagnostics.CodeAnalysis.SuppressMessage("Microsoft.Usage", "CA2214:DoNotCallOverridableMethodsInConstructors")]
@@ -21,6 +22,8 @@ namespace Mvcstok.Models.Entity
         }
     
         public short kategoriid { get; set; }
+
+        [Required]
         public string kategoriad { get; set; }
     
         [System.Diagnostics.CodeAnalysis.SuppressMessage("Microsoft.Usage", "CA2227:CollectionPropertiesShouldBeReadOnly")]
